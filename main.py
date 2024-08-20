5dc501e74c708236850837d635cfbecff73eb935
Dolyfin <Dolyfin.iz@gmail.com>
2024-08-20 13:04:46 +1000

    stepper update

--- main.py
+++ main.py
@@ -1,45 +1,34 @@
 import time
-from HR8825 import HR8825
+import stepper
+import readline  # This enables command history
 
-Motor1 = HR8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
-Motor2 = HR8825(dir_pin=24, step_pin=18, enable_pin=4, mode_pins=(21, 22, 27))
+def main():
+    try:
+        while True:
+            # Read input with history support
+            command_input = input("> ")
 
+            # Split the input into parts
+            parts = command_input.split()
 
-def set_motor1_angle(steps, speed):
-    # stepdelay = (9 - speed) * 0.001
-
-    Motor1.SetMicroStep('softward', 'fullstep')
-    Motor1.TurnStep(Dir='forward', steps=steps, stepdelay=speed)
-
-
-def set_motor2_angle(steps, speed):
-    Motor2.SetMicroStep('softward', 'fullstep')
-    Motor2.TurnStep(Dir='forward', steps=steps, stepdelay=speed)
-
-
-if __name__ == "__main__":
-    while True:
-        command_input = input(">")
-
-        parts = command_input.split()
-
-        if parts[0].startswith('m') and len(parts) == 3:
-            if len(parts) == 3:
+            if parts[0].startswith('m') and len(parts) == 3:
                 motor = parts[0]
                 steps = int(parts[1])
                 speed = float(parts[2])
-                # if speed > 9:
-                #     speed = 9
-                #
-                # if speed < 0:
-                #     speed = 0
-
 
                 if motor == "m1":
-                    set_motor1_angle(steps, speed)
+                    stepper.m1_move(steps, speed, True)
                 elif motor == "m2":
-                    set_motor2_angle(steps, speed)
+                    stepper.m2_move(steps, speed, False)
                 else:
                     print("Unknown motor command")
             else:
                 print("Invalid command format. Use: m1 <steps> <speed> or m2 <steps> <speed>. Speed 0-9.")
+    except KeyboardInterrupt:
+        print("\nExiting...")
+    finally:
+        stepper.cleanup()
+
+
+if __name__ == "__main__":
+    main()
---
+++ stepper.py
@@ -0,0 +1,76 @@
+import RPi.GPIO as GPIO
+import time
+
+# motor 1 pins
+m1int1 = 17
+m1in2 = 18
+m1in3 = 27
+m1in4 = 22
+
+# motor 2 pins
+m2in1 = 5
+m2in2 = 6
+m2in3 = 13
+m2in4 = 19
+
+# careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
+step_sleep = 0.002
+
+step_count = 4096  # 5.625*(1/64) per step, 4096 steps is 360°
+
+# defining stepper motor sequence (found in documentation http://www.4tronix.co.uk/arduino/Stepper-Motors.php)
+step_sequence = [[1, 0, 0, 1],
+                 [1, 0, 0, 0],
+                 [1, 1, 0, 0],
+                 [0, 1, 0, 0],
+                 [0, 1, 1, 0],
+                 [0, 0, 1, 0],
+                 [0, 0, 1, 1],
+                 [0, 0, 0, 1]]
+
+# setting up
+m1_pins = [m1int1, m1in2, m1in3, m1in4]
+m1_step_counter = 0
+
+for pin in m1_pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.LOW)
+
+m2_pins = [m2in1, m2in2, m2in3, m2in4]
+m2_step_counter = 0
+
+for pin in m2_pins:
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.LOW)
+
+
+def m1_move(steps, delay=0.005, direction=True):
+    global m1_step_counter
+    for i in range(steps):
+        for pin in range(0, len(m1_pins)):
+            GPIO.output(m1_pins[pin], step_sequence[m1_step_counter][pin])
+        if direction == True:
+            m1_step_counter = (m1_step_counter - 1) % 8
+        elif direction == False:
+            m1_step_counter = (m1_step_counter + 1) % 8
+        time.sleep(delay)
+
+
+def m2_move(steps, delay=0.005, direction=True):
+    global m2_step_counter
+    for i in range(steps):
+        for pin in range(0, len(m2_pins)):
+            GPIO.output(m2_pins[pin], step_sequence[m2_step_counter][pin])
+        if direction == True:
+            m2_step_counter = (m2_step_counter - 1) % 8
+        elif direction == False:
+            m2_step_counter = (m2_step_counter + 1) % 8
+        time.sleep(delay)
+
+
+def cleanup():
+    for pin in m1_pins:
+        GPIO.output(pin, GPIO.LOW)
+    for pin in m2_pins:
+        GPIO.output(pin, GPIO.LOW)
+    GPIO.cleanup()
