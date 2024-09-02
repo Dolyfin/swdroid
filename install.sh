dfdc5892597a915edd8b4c5548f5081537c3600c
Dolyfin <Dolyfin.iz@gmail.com>
2024-09-02 21:15:52 +1000

    agent test

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 # install.sh
-source venv/bin/activate
 export CMAKE_ARGS="-DGGML_BLAS=ON;-DGGML_BLAS_VENDOR=OpenBLAS"
 pip install -r requirements.txt
 pip install --no-cache-dir --force-reinstall llama-cpp-python
